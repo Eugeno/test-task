3da329d2a67f762aff46601808416ea469cdfcbf
eugene <eugeneath@ya.ru>
2024-07-09 23:46:44 +0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,83 @@
+import { NgFor } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import 'zone.js';
+import { Item, UserService } from './user.service';
 
-bootstrapApplication(AppComponent, appConfig)
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgFor],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <button type="button" (click)="add()">add</button>
+    <table>
+      <tr *ngFor="let item of items">
+        <td>{{item.name}}</td>
+        <td>{{item.amount}}</td>
+        <td>{{item.emoji}}</td>
+      </tr>
+    </table>
+    <div>
+      <b>total:</b>
+      {{total}}
+    </div>
+
+    <hr>
+
+    <p>
+      <input #input type="text" (input)="search(input.value)">
+      @if (result) {
+        {{result.name}} — {{result.amount}}
+      }
+    </p>
+  `,
+})
+export class App implements OnInit {
+  userService = inject(UserService);
+
+  items: Item[] = [];
+  total!: number;
+  result?: Item;
+
+  ngOnInit() {
+    this.getUsers();
+    this.total = this.items.reduce((acc, item) => acc + item.amount, 0);
+  }
+
+  add() {
+    this.items.push({
+      name: 'Mike',
+      amount: 99,
+      emoji: '✨',
+    });
+  }
+
+  search(name: string) {
+    this.userService.search(name)
+      .subscribe((user) => {
+        if (user) {
+          this.result = user;
+        }
+      })
+  }
+
+  getUsers() {
+    this.userService.getUsers().subscribe((users) => {
+      this.items = users.reduce((acc: Item[], item) => {
+        console.log(acc);
+        acc.push()
+        return [
+          ...acc,
+          {
+            ...item,
+            emoji: item.amount < 20 ? '🙁' : item.amount < 30 ? '😐' : '🙂',
+          }
+        ];
+      }, []);
+    });
+  }
+}
+
+bootstrapApplication(App)
   .catch((err) => console.error(err));
---
+++ src/user.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { map, Observable, timer } from "rxjs";
+
+export interface Item {
+  name: string;
+  amount: number;
+  emoji?: string;
+}
+
+const USERS: Item[] = [
+  {
+    name: 'John',
+    amount: 31,
+  },
+  {
+    name: 'Dave',
+    amount: 14,
+  },
+  {
+    name: 'Alex',
+    amount: 27,
+  },
+]
+
+@Injectable()
+export class UserService {
+  getUsers(): Observable<Item[]> {
+    return timer(999).pipe(map(() => [...USERS]));
+  }
+
+  search(name: string): Observable<Item | null> {
+    return timer(this.getRandomInt(111, 999)).pipe(map(() => USERS.find((user) => user.name.includes(name)) || null));
+  }
+
+  private getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (min - max + 1)) + min;
+  };
+}
